1140c05983c1d40cc061d0a8e185fd3402482594
Massimo Di Pierro <massimo.dipierro@gmail.com>
2023-04-10 22:29:24 -0700

    added comments

--- week2/apps/my_second_app/controllers.py
+++ week2/apps/my_second_app/controllers.py
@@ -4,24 +4,25 @@ from py4web.utils.form import Form
 from pydal.validators import IS_NOT_EMPTY
 
 @action("index")
-@action.uses("index.html", session)
+@action.uses("index.html", auth)            # has access to auth
 def index():
     return dict(message="hello")
 
 @action("counter")
 @action.uses(session)
 def counter():
-    session["c"] = session.get("c", 0) + 1
+    session["c"] = session.get("c", 0) + 1  # add 1 to session["c"]
     return str(session["c"])
 
 @action("hello")
-@action.uses("generic.html", auth.user)
+@action.uses("generic.html", auth.user)    # requires an auth(enticated) user
 def hello():    
     return dict(message=f"Hello")
 
-@action("todo", method=["GET", "POST"])
-@action.uses("todo.html", auth.user)
+@action("todo", method=["GET", "POST"])    # can process POSTed forms
+@action.uses("todo.html", auth.user)       # requires an auth(enticated) user
 def todo():    
-    form = Form(db.todo)
-    items = db(db.todo).select()
+    form = Form(db.todo)                   # make a create form for table db.todo
+    items = db(db.todo).select()           # select records/items from table
     return dict(form=form, items=items)
+
