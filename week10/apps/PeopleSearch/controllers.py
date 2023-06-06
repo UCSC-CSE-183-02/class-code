ab177a3515b53f954c1952f16f8462b51101b498
Massimo Di Pierro <massimo.dipierro@gmail.com>
2023-06-05 20:12:01 -0700

    version 3 of peoplesearch

--- week10/apps/PeopleSearch/controllers.py
+++ week10/apps/PeopleSearch/controllers.py
@@ -1,3 +1,4 @@
+import functools
 from py4web import action, request, abort, redirect, URL
 from yatl.helpers import A
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
@@ -11,6 +12,16 @@ def index():
 @action("api/users")
 @action.uses(db)
 def api_users():
-    users = db(db.auth_user).select(db.auth_user.id, db.auth_user.first_name, db.auth_user.last_name)
+    q = request.query.get("q");
+    if q is None:
+        query = db.auth_user
+    else:
+        queries = []
+        for word in q.lower().split():
+            queries.append(db.auth_user.first_name.contains(word) | 
+                           db.auth_user.last_name.contains(word))
+        query = functools.reduce(lambda a,b: a & b, queries)
+        
+    users = db(query).select(db.auth_user.id, db.auth_user.first_name, db.auth_user.last_name)
     return dict(users = users.as_list())
 
--- week10/apps/PeopleSearch/static/js/index.js
+++ week10/apps/PeopleSearch/static/js/index.js
@@ -5,27 +5,14 @@ function init() {
     self.data = {};    
     self.methods = {};
     self.data.search_string = "";
-    self.data.users = [];
     self.data.filtered_users = [];
-    self.methods.filter_users = function() {
-        self.vue.filtered_users = [];
-        if (self.vue.search_string.length<3) return;
-        var words = self.vue.search_string.toLowerCase().split(" ");
-        self.vue.users.map(function(user){
-            for(var i=0; i<words.length; i++) {
-                var word = words[i];
-                if (user.first_name.toLowerCase().includes(word) ||
-                    user.last_name.toLowerCase().includes(word)) {
-                        self.vue.filtered_users.push(user);
-                    }
-            }
+    self.methods.filter_users = Q.debounce(function() {
+        fetch("../api/users?q="+self.vue.search_string).then(r=>r.json())
+        .then(function(response){
+            self.vue.filtered_users = response.users;
         });
-    };
+    }, 1000);
     self.vue = new Vue({el:"#vue", data:self.data, methods:self.methods})    
-    fetch("../api/users").then(r=>r.json()).then(function(response){        
-        self.vue.users = response.users;
-        // self.methods.filter_users();
-    });
     return self;
 }
 
