c418ae59b7bb76077207d843f53e126774169e87
Massimo Di Pierro <massimo.dipierro@gmail.com>
2023-06-05 20:00:53 -0700

    version 2 of peoplesearch

--- week10/apps/PeopleSearch/static/js/index.js
+++ week10/apps/PeopleSearch/static/js/index.js
@@ -6,20 +6,25 @@ function init() {
     self.methods = {};
     self.data.search_string = "";
     self.data.users = [];
-    self.methods.search = function(search_string, user) {
-        var words = search_string.toLowerCase().split(" ");
-        for(var i=0; i<words.length; i++) {
-            var word = words[i];
-            if (!user.first_name.toLowerCase().includes(word) &&
-                !user.last_name.toLowerCase().includes(word)) {
-                    return false;
-                }
-        }
-        return true;
+    self.data.filtered_users = [];
+    self.methods.filter_users = function() {
+        self.vue.filtered_users = [];
+        if (self.vue.search_string.length<3) return;
+        var words = self.vue.search_string.toLowerCase().split(" ");
+        self.vue.users.map(function(user){
+            for(var i=0; i<words.length; i++) {
+                var word = words[i];
+                if (user.first_name.toLowerCase().includes(word) ||
+                    user.last_name.toLowerCase().includes(word)) {
+                        self.vue.filtered_users.push(user);
+                    }
+            }
+        });
     };
-    self.vue = new Vue({el:"#vue", data:self.data, methods:self.methods})
+    self.vue = new Vue({el:"#vue", data:self.data, methods:self.methods})    
     fetch("../api/users").then(r=>r.json()).then(function(response){        
         self.vue.users = response.users;
+        // self.methods.filter_users();
     });
     return self;
 }
